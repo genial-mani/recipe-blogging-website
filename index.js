85046928b20bd5f7b3ceab3881feeeada9856588
genial-mani <manikantaginige1@gmail.com>
2024-06-14 18:21:20 +0530

    solved deploy error2

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ app.use(cors({
   origin: ['http://localhost:3000', 'https://recipe-blogging-website-frontend.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Access-Control-Allow-Origin']
 }));
 
 // Middleware
